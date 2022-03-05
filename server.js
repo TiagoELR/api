a3facf13f7f7fd665c6151f3b6260248bf761b81
TiagoELR <tiagoelr@hotmail.com>
2022-03-05 11:40:12 -0300

    update port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 const app = require("./app");
 var server = http.createServer(app);
 
-server.listen(8081);
+server.listen(8080);
 
 console.log("Server running at http://127.0.0.1:8081/");
