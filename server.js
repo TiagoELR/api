d76688fd1a1b9f9f88753b38524aaf8d6fbf6fb0
TiagoELR <tiagoelr@hotmail.com>
2022-03-06 10:10:41 -0300

    fix PORT to 5000

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 const app = require("./app");
 var server = http.createServer(app);
 
-server.listen(8080);
+server.listen(process.env.PORT || 5000);
 
 console.log("Server running at http://127.0.0.1:8081/");
